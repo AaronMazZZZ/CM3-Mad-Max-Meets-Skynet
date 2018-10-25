5a7a36964ec3577960a5e0384237de02a3ca29c3
AaronMazZZZ <37770518+AaronMazZZZ@users.noreply.github.com>
2018-10-26 10:18:17 +1100

    Add files via upload

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/aaron/racecar_ws4.1/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/aaron/catkin_ws/devel;/home/aaron/catkin_mit/devel;/opt/ros/kinetic".split(';') if "/home/aaron/catkin_ws/devel;/home/aaron/catkin_mit/devel;/opt/ros/kinetic" != "" else []
