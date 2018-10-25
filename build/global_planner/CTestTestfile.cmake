857797961ad4946920519394b5c6df4427ad5afd
AaronMazZZZ <37770518+AaronMazZZZ@users.noreply.github.com>
2018-10-26 10:21:34 +1100

    Add files via upload

---
+++ build/global_planner/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/aaron/racecar_ws4.1/src/global_planner
+# Build directory: /home/aaron/racecar_ws4.1/build/global_planner
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/global_planner/setup_custom_pythonpath.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+# generated from dynamic_reconfigure/cmake/setup_custom_pythonpath.sh.in
+
+PYTHONPATH=/home/aaron/racecar_ws4.1/devel/lib/python2.7/dist-packages:$PYTHONPATH
+exec "$@"
