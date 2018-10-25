f6e9f75a6ca84d1375007105eb13befc819340b6
AaronMazZZZ <37770518+AaronMazZZZ@users.noreply.github.com>
2018-10-26 10:44:51 +1100

    Add files via upload

---
+++ build/open_karto/CMakeFiles/karto.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/karto.dir/src/Karto.cpp.o"
+  "CMakeFiles/karto.dir/src/Mapper.cpp.o"
+  "/home/aaron/racecar_ws4.1/devel/lib/libkarto.pdb"
+  "/home/aaron/racecar_ws4.1/devel/lib/libkarto.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/karto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
