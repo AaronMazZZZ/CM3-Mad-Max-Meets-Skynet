3c9868e63039ba6e620e2daf6b557aa5b7445e50
AaronMazZZZ <37770518+AaronMazZZZ@users.noreply.github.com>
2018-10-26 09:57:56 +1100

    Add files via upload

---
+++ build/ackermann_msgs/CMakeFiles/ackermann_msgs_generate_messages_eus.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ackermann_msgs_generate_messages_eus"
+  "/home/aaron/racecar_ws4.1/devel/share/roseus/ros/ackermann_msgs/msg/AckermannDrive.l"
+  "/home/aaron/racecar_ws4.1/devel/share/roseus/ros/ackermann_msgs/msg/AckermannDriveStamped.l"
+  "/home/aaron/racecar_ws4.1/devel/share/roseus/ros/ackermann_msgs/manifest.l"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/ackermann_msgs_generate_messages_eus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
