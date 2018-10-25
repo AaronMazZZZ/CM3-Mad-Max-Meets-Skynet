f9658c89fc147502acb821a581616f66883f6885
AaronMazZZZ <37770518+AaronMazZZZ@users.noreply.github.com>
2018-10-26 10:07:20 +1100

    Add files via upload

---
+++ build/amcl/CMakeFiles/amcl_gencfg.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/amcl_gencfg"
+  "/home/aaron/racecar_ws4.1/devel/include/amcl/AMCLConfig.h"
+  "/home/aaron/racecar_ws4.1/devel/share/amcl/docs/AMCLConfig.dox"
+  "/home/aaron/racecar_ws4.1/devel/share/amcl/docs/AMCLConfig-usage.dox"
+  "/home/aaron/racecar_ws4.1/devel/lib/python2.7/dist-packages/amcl/cfg/AMCLConfig.py"
+  "/home/aaron/racecar_ws4.1/devel/share/amcl/docs/AMCLConfig.wikidoc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/amcl_gencfg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
