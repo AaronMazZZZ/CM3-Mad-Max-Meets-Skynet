fbc59a94159d7bbf0d86d60b295fa2982c9bf7e9
AaronMazZZZ <37770518+AaronMazZZZ@users.noreply.github.com>
2018-10-26 11:08:51 +1100

    Add files via upload

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,22 @@
+# CMake generated Testfile for 
+# Source directory: /home/aaron/racecar_ws4.1/src
+# Build directory: /home/aaron/racecar_ws4.1/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(open_karto)
+subdirs(racecar/racecar)
+subdirs(racecar-simulator/racecar_control)
+subdirs(racecar-simulator/racecar_description)
+subdirs(ackermann_msgs)
+subdirs(map_server)
+subdirs(racecar/ackermann_cmd_mux)
+subdirs(amcl)
+subdirs(racecarDepth)
+subdirs(racecar-simulator/racecar_gazebo)
+subdirs(slam_karto)
+subdirs(trajectory_tracking)
+subdirs(global_planner)
+subdirs(racecar_local_planner)
+subdirs(move_base)
